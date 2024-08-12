3414e83435ce3f89291c7674415c22634e27e1af
programingCat <edwin.velasquez@umanizales.edu.co>
2024-08-12 18:43:06 -0500

    Menu hamburguesa responsive

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,16 +1,24 @@
 import {React, useState }  from 'react';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileInvoice } from '@fortawesome/free-solid-svg-icons';
+import { faFileInvoice, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
     //Activacion de la opcion del navbar al dar click
     const location = useLocation();
     const [activeItem, setActiveItem] = useState(location.pathname === '/' ? 'Clientes' : 'Facturas');
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+    // Cierra el menú después de seleccionar una opción
     const handleItemClick = (item) => {
         setActiveItem(item);
+        setIsMenuOpen(false);
+    };
+
+    //Mantiene oculto el menú
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
     };
 
     return (
@@ -19,7 +27,7 @@ const Navbar = () => {
                 <FontAwesomeIcon icon={faFileInvoice} className="logo-icon" />
                 <span className="logo-text">FACTURATE</span>
             </div>
-            <div className="menu">
+            <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
                 <Link
                     to="/"
                     className={`menu-item ${activeItem === 'Clientes' ? 'active' : ''}`}
@@ -35,6 +43,9 @@ const Navbar = () => {
                     Facturas
                 </Link>
             </div>
+            <button className="hamburger" onClick={toggleMenu}>
+                <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+            </button>
         </nav>
     );
 };
